d8cc5395144a598acc6d4349ba285bc2a47aac80
jorektheglitch <57464345+jorektheglitch@users.noreply.github.com>
2020-07-19 01:10:38 +0300

    Add files via upload

---
+++ shiffer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import asyncio
+
+from socket import socket, AF_PACKET, SOCK_DGRAM
+from socket import htons
+
+import argparse
+from sys import argv
+
+ETH_P_ALL = 0x0003
+
+
+def stop():
+    print('\nstoping sniffer...')
+    loop = asyncio.get_event_loop()
+    loop.stop()
+    print('sniifer stopped')
+    exit()
+
+
+class RawProtocol:
+
+    def connection_made(self, transport:asyncio.DatagramTransport):
+        self.transport = transport
+
+    def datagram_received(self, data, source):
+        #ifacename, protocol_type, in or out, packet_type ETHERNET, hwaddr
+        if_name, proto, direction, pk_type, src_mac = source
+        message = data.decode()
+        mac_str = ':'.join(hex(i)[2:].upper() for i in src_mac)
+        print('{}:\n{}\n\n'.format(mac_str, message))
+
+    def error_received(self, err):
+        print('ERROR!\n{}: {}'.format(err.__class__.__name__, str(err)))
+        stop()
+
+    def connection_lost(self, exc): 
+        print('device not available now')
+        stop()
+
+
+async def start_sniff(if_name):
+    loop = asyncio.get_event_loop()
+    raw_sock = socket(AF_PACKET, SOCK_DGRAM)
+    raw_sock.bind((if_name, ETH_P_ALL))
+    raw_reader, raw_writer = await loop.create_datagram_endpoint(
+        RawProtocol, 
+        sock=raw_sock
+    )
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='Simple asyncio sniffer', add_help=True)
+    parser.add_argument('-i', '--ifname', default=None, help='Name of interface which sniffer will run on')
+    namespace = parser.parse_args(argv[1:])
+    ifname = namespace.ifname
+
+    if not ifname:
+        print("Interface name don't specified")
+        exit()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_sniff(ifname))
+    
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        stop()
+    except Exception as e:
+        print('ERROR!\n{}: {}'.format(e.__class__.__name__, str(e)))
+        stop()
